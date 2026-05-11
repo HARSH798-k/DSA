bea0950216de6d46f17d4acf079057d87b75d55d
Harsh Kataria <katariaharsh981@gmail.com>
2026-05-11 20:41:00 +0530

    Star pattern

---
+++ Star.java
@@ -0,0 +1,13 @@
+// Star pattern
+class Star{
+    static void star(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+    public static void main(String Args[]){
+        star(5);
+    }
+}
