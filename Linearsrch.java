e4282039be5b0e094349b038f676668d7bb910a1
Harsh Kataria <katariaharsh981@gmail.com>
2026-05-15 00:18:21 +0530

    last commit

--- Linearsrch.java
+++ Linearsrch.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 public class Linearsrch {
     static void linearsrch(int num[], int key){
+        boolean found = false;
         for(int i = 0; i < num.length; i++){
             if(num[i] == key){
                 System.out.println("Key at index : " + i);
+                found = true;
                 break; 
-            }else{
-                 System.out.println("Key not in array.");
             }
+        }  
+        if(!found){
+            System.out.println("Key is not in array.");
         }
-        
-        
-       
     }
     
     public static void main(String Args[]){
         int num[] = {2, 3, 5, 6, 8, 10, 34, 55, 67, 23};
-        int key = 34;
+        int key = 1;
         linearsrch(num, key);
     }
 }
